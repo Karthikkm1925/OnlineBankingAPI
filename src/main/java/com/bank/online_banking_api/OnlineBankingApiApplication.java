5827c8bf726529f196edd99a1ec542d2f3701142
Karthikkm1925 <Karthikkm1925@gmail.com>
2026-02-02 01:31:29 +0530

    Initial Commit : project structure created and updated gitignore and .gitattributes alomg pom.xml (pom.xml), yml file

---
+++ src/main/java/com/bank/online_banking_api/OnlineBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.bank.online_banking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingApiApplication.class, args);
+	}
+
+}
